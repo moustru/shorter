719fc18052486504a27ebc0945b13737c7f546a8
Eugene <xexyz@yandex.ru>
2026-03-19 16:16:55 +0000

    Фиксы при деплое

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+app.enableCors({
+    origin: [
+      'http://5.42.126.153',      // без порта (80)
+      'http://5.42.126.153:8080',  // с портом 8080
+      'http://localhost:8080',     // для разработки
+      'http://localhost'           // для разработки
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+});
 
   app.setGlobalPrefix('api');
 
